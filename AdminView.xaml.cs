eaebc958c169e289095698d111e8ff33b4f303ca
asivadasan45 <160364835+asivadasan45@users.noreply.github.com>
2024-06-21 12:39:43 -0400

    Admin Dashboard

---
+++ AdminView.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Digident
+{
+    public partial class AdminView : Page
+    {
+        public List<DataItem> DataItems { get; set; }
+
+        public AdminView()
+        {
+            InitializeComponent();
+
+            // Initialize data
+            DataItems = new List<DataItem>
+            {
+                new DataItem { FieldName = "First Name", FieldValue = "" },
+                new DataItem { FieldName = "Last Name", FieldValue = "" },
+                new DataItem { FieldName = "Appointment Date and Time", FieldValue = "" },
+                new DataItem { FieldName = "Reason for Visit", FieldValue = "" }
+            };
+
+            // Set data context for the ListView
+            DataContext = this;
+        }
+    }
+
+    // Data class for binding
+    public class DataItem
+    {
+        public string FieldName { get; set; }
+        public string FieldValue { get; set; }
+    }
+}
